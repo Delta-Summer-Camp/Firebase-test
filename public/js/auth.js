c334e8f2c520e9e5731c00504a31b90264983427
Dima <d.ablov@gmail.com>
2020-07-22 19:58:17 +0300

    Start project

--- public/js/auth.js
+++ public/js/auth.js
@@ -6,6 +6,21 @@ function isAuth() {
     firebase.auth().onAuthStateChanged((user) => {
         if(user == null) {
             document.getElementById('login-block').style.display = 'block';
+        } else {
+            document.getElementById('login-block').style.display = 'none';
         }
     });
+}
+
+function login() {
+    const email = document.getElementById('email').value;
+    const passwd = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, passwd)
+        .catch((error) => {
+            alert(error.message);
+            document.querySelector('#password').value = '';
+            return false;
+        });
+    return true;
 }
