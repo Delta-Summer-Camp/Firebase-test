4f1f4a22e24076c6dc0206380b4f778b93b87a7b
Dima <d.ablov@gmail.com>
2020-07-24 18:22:40 +0300

    Start project

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,5 @@
 function logoff() {
-    alert('Are you sure?');
+    firebase.auth().signOut();
 }
 
 function isAuth() {
@@ -8,6 +8,7 @@ function isAuth() {
             document.getElementById('login-block').style.display = 'block';
         } else {
             document.getElementById('login-block').style.display = 'none';
+            document.getElementById('reg-block').style.display = 'none';
         }
     });
 }
@@ -23,4 +24,33 @@ function login() {
             return false;
         });
     return true;
+}
+
+function showReg() {
+    document.querySelector('#reg-block').style.display = 'block';
+    document.querySelector('#login-block').style.display = 'none';
+}
+
+function register() {
+    const email = document.querySelector('#reg-email').value;
+    const nickname = document.querySelector('#reg-nickname').value;
+    const passwd = document.querySelector('#reg-password').value;
+    const passwd2 = document.querySelector('#reg-password2').value;
+
+    if (passwd !== passwd2) {
+        alert ('Пароли не совпадают!');
+        return false;
+    } else {
+        firebase.auth().createUserWithEmailAndPassword(email, passwd)
+            .then(()=>{
+                let user = firebase.auth().currentUser;
+                user.updateProfile({
+                    displayName: nickname
+                })
+            })
+            .catch((error)=> {
+                alert(error.message);
+                return false;
+            });
+    }
 }
